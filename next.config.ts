5999fc1a5698bbd5aa38e0ae3483af0d62cc12ce
hsg8-commits <hsjdgg8@gmail.com>
2025-12-01 05:46:22 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,37 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // يسمح بالتجميع حتى لو كان هناك تحذيرات/أخطاء ESLint
   },
+  // Performance optimizations
+  swcMinify: true,
+  compress: true,
+  // Enable gzip compression
+  experimental: {
+    optimizeCss: true,
+    optimizeServerReact: true,
+  },
+  // Webpack optimizations
+  webpack: (config, { isServer }) => {
+    // Optimize bundle size
+    config.optimization = {
+      ...config.optimization,
+      splitChunks: {
+        ...config.optimization.splitChunks,
+        cacheGroups: {
+          ...config.optimization.splitChunks?.cacheGroups,
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
+          },
+        },
+      },
+    };
+    
+    // Enable tree shaking
+    config.optimization.usedExports = true;
+    
+    return config;
+  },
   images: {
     remotePatterns: [
       // أي فرع فرعي على vercel.app
